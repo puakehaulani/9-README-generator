d1cca85802dc1ce78300b81bc7f90653f0c3532f
puakehaulani <puakehaulani@gmail.com>
2020-10-31 00:02:06 -0700

    added in markdown code, mvp achieved. formatting bugs addressed

--- index.js
+++ index.js
@@ -56,12 +56,67 @@ const promptUser = () =>
     ]).then((answers) => {
         console.log(answers);
         //insert if statement for checking badge
-        // const markdownString = `
-        //
-        //`;
-        //fs.writeFile("README.md", markdownString, err => {
-        //         if (err) console.log(err)
-        //     })
+        //make title into a string with - instead of spaces to be used in links
+        const markdownString = `# ${answers.title}
+
+![screenshot]()
+
+[See the project](http://${answers.name}.github.io/${answers.title})  
+[Explore the docs](http://github.com/${answers.name}/${answers.title})</div>
+
+---
+
+## Table of Contents
+
+- [About the Project](#About-the-Project)
+- [Usage](#Usage)
+- [Getting Started](#Getting-Started)
+  - [Installation](#Installation)
+- [Contributing](#Contributing)
+- [Testing](#Testing)
+- [Contact](#Contact)
+- [License](#License)  
+
+## About the Project  
+
+${answers.description}  
+
+## Usage  
+
+${answers.usage}  
+
+## Getting Started  
+
+To get started, follow these steps:  
+
+### Installation  
+
+Run the following command in your terminal  
+
+    ${answers.dependencies}  
+
+## Testing
+
+Run the following command in your terminal  
+
+    ${answers.test}  
+
+## Contributing
+
+${answers.contribute}
+
+## Contact
+
+<${answers.email}>
+
+## License
+
+${answers.license}
+
+        `;
+        fs.writeFile("README.md", markdownString, err => {
+            if (err) console.log(err)
+        })
     })
 
 // function call to initialize program
